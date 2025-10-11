4fde20df9df9c2ac6e653d8652a5a8fb863edbc1
Subash B <thapasubash9072@gmail.com>
2025-10-11 11:51:27 +0530

    changes made

--- src/components/pages/Home.jsx
+++ src/components/pages/Home.jsx
@@ -3,9 +3,10 @@ import React from 'react'
 const Home = () => {
   return (
     <>
-     <section className="relative min-h-screen bg-gradient-to-t from-blue-600 to-red-600  text-white flex flex-col md:flex-row items-center justify-between px-8 md:px-20 py-20 overflow-hidden">
+     <section className="relative min-h-screen bg-black text-white flex flex-col md:flex-row items-center justify-between px-8 md:px-20 py-20 overflow-hidden">
       {/* Background */}
-
+      <div className="absolute inset-0 bg-[url('/columns-bg.jpg')] bg-cover bg-center opacity-30"></div>
+      <div className="absolute inset-0 bg-gradient-to-t from-black via-transparent to-black opacity-80"></div>
 
       {/* Left content */}
       <div className="relative z-10 max-w-xl space-y-6">
